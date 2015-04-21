137c270983682708cd9e231cdc7342c8e7fbbef6
H3adsho0ot <niklas.roll123@gmail.com>
2015-04-21 13:44:14 +0200

    Created Project

---
+++ KuhlEngineTester/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace KuhlEngineTester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.DarkRed;
+            this.pictureBox1.Location = new System.Drawing.Point(36, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(280, 173);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(407, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "0";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(388, 146);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "bu";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(494, 143);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 10000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 323);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ KuhlEngineTester/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using KuhlEngine;
+
+using System.IO;
+using System.Threading;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace KuhlEngineTester
+{
+    public partial class Form1 : Form
+    {
+        public delegate void emptyFunction();
+
+        Renderer renderer = new Renderer();
+        Texture mBackTexture;
+
+        Image gifImage = new Bitmap(@"C:\Users\Roll\Desktop\1.gif");
+        int currentFrame = -1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Renderer.newFrame += new Renderer.RenderHandler(rendererEvent);
+            mBackTexture = new Texture(@"E:\Eigene Daten\Pictures\PNGs 150px\0.png");
+            mBackTexture.Stretch = false;
+            renderer.initializeMap(300, 200, mBackTexture);
+        }
+
+        private void renderGif()
+        {
+            FrameDimension dimension = new FrameDimension(gifImage.FrameDimensionsList[0]);
+            int frameCount = gifImage.GetFrameCount(dimension);
+
+            currentFrame += 1;
+
+            if (currentFrame >= frameCount)
+            {
+                currentFrame = Math.Abs(currentFrame - frameCount);
+            }
+            gifImage.SelectActiveFrame(dimension, currentFrame);
+            Bitmap curGif = new Bitmap(gifImage.Width, gifImage.Height);
+
+            Graphics g = Graphics.FromImage(curGif);
+            g.DrawImage((Bitmap)gifImage.Clone(), new Point(0, 0));
+            g.Dispose();
+
+            pictureBox1.Width = gifImage.Width;
+            pictureBox1.Height = gifImage.Height;
+            pictureBox1.Image = curGif;
+            
+        }
+
+        private void rendererEvent(Image aFrame, int aWidth, int aHeight)
+        {
+            //pictureBox1.Width = aWidth;
+            //pictureBox1.Height = aHeight;
+            //pictureBox1.Image = aFrame;
+            if (this.InvokeRequired) this.Invoke(new emptyFunction(delegate() { label1.Text = Convert.ToString(Convert.ToInt32(label1.Text) + 1); }));
+                
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            renderGif();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(textBox1.Text);
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            timer2.Enabled = false;
+            textBox1.Text = Convert.ToString(Convert.ToDouble(label1.Text) / 10);
+        }
+    }
+}
